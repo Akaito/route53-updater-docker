95632d61ea0995aa3fd45171e1283005fefe9a39
Chris Barrett <chris@codesaru.com>
2019-03-09 10:15:25 -0800

    Assert on missing env vars.  Tweaks/cleanup.

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ import botocore.session
 
 # required env vars
 HOSTED_ZONE_ID = os.getenv('R53_HOSTED_ZONE_ID', None)  # TODO get config'd zone ID string (such as for codesaru.com)
-DNS_NAME       = os.getenv('R53_DNS_NAME', None)  # TODO get from config (such as 'home.codesaru.com')
+DNS_NAME       = os.getenv('DNS_NAME', None)  # TODO get from config (such as 'home.codesaru.com')
 
 # optional env vars
 PUBLIC_IP_URL = os.getenv('PUBLIC_IP_URL', 'http://checkip.amazonaws.com')
-TTL_SECONDS   = os.getenv('R53_TTL_SECONDS', 300)
+TTL_SECONDS   = int(os.getenv('TTL_SECONDS', '300'))
+
+
+assert HOSTED_ZONE_ID, "Route53's Hosted Zone ID should be set to env var HOSTED_ZONE_ID."
+assert DNS_NAME, "The DNS name to update A records for should be set to env var DNS_NAME."
 
 
 # it's expected that you've volume-mounted a file with only the creds this container should use
@@ -25,6 +29,7 @@ def update_ip():
     f = urllib.urlopen(PUBLIC_IP_URL)
     assert f.getcode() == 200, 'Failed to get public IP'
     public_ip = f.read().strip()
+    #print 'Discovered public IP: ' + public_ip
 
 
     # update Route 53
@@ -36,9 +41,9 @@ def update_ip():
                     {
                         'Action': 'UPSERT',
                         'ResourceRecordSet': {
-                            'Name': DNS_NAME,
+                            'Name': str(DNS_NAME),
                             'Type': 'A',
-                            'TTL': TTL_SECONDS,
+                            'TTL': int(TTL_SECONDS),
                             'ResourceRecords': [
                                 { 'Value': public_ip }
                             ]
@@ -49,7 +54,7 @@ def update_ip():
                         'ResourceRecordSet': {
                             'Name': '*.' + str(DNS_NAME),
                             'Type': 'A',
-                            'TTL': TTL_SECONDS,
+                            'TTL': int(TTL_SECONDS),
                             'ResourceRecords': [
                                 { 'Value': public_ip }
                             ]
