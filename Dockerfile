392fbe980e728594c27a3a61fe7d7d37f37484ba
Chris Barrett <chris@codesaru.com>
2019-03-09 09:18:21 -0800

    WIP rework for easier unRaid use.

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,14 @@ RUN apk --update add py2-pip \
 	&& pip install awscli \
 	&& rm -rf /var/cache/apk/*
 
-#RUN rm -rf /etc/nginx/conf.d/*
-#COPY nginx.conf /etc/nginx/nginx.conf
-#COPY conf.d /etc/nginx/conf.d
-COPY aws /root/.aws
-COPY main.py /
+RUN groupadd -r route53 \
+    && useradd --no-log-init -r -m -g route53 route53
+USER route53:route53
 
+#COPY aws /root/.aws
+COPY main.py /home/route53/
+
+RUN chmod -R o+rx /home/route53
 #CMD ["/bin/sh"]  # for debugging
 CMD [ "python", "./main.py" ]
 #CMD cron && tail -f /var/log/cron.log
--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python
 
+import os
 import urllib
 
 import botocore
 import botocore.session
 
+# required env vars
+HOSTED_ZONE_ID = os.getenv('R53_HOSTED_ZONE_ID', None)  # TODO get config'd zone ID string (such as for codesaru.com)
+DNS_NAME       = os.getenv('R53_DNS_NAME', None)  # TODO get from config (such as 'home.codesaru.com')
 
-session = botocore.session.get_session()  # TODO get access key/secret (or profile name?)
+# optional env vars
+PUBLIC_IP_URL = os.getenv('PUBLIC_IP_URL', 'http://checkip.amazonaws.com')
+TTL_SECONDS   = os.getenv('R53_TTL_SECONDS', 300)
+
+
+# it's expected that you've volume-mounted a file with only the creds this container should use
+session = botocore.session.get_session()
 route53_client = session.create_client('route53')
-
-ttl = 300
-HOSTED_ZONE_ID = None  # TODO get config'd zone ID string (such as for codesaru.com)
-DNS_NAME = None  # TODO get from config (such as 'home.codesaru.com')
 
 
 def update_ip():
     # get WAN IP
-    f = urllib.urlopen('http://checkip.amazonaws.com')
+    f = urllib.urlopen(PUBLIC_IP_URL)
     assert f.getcode() == 200, 'Failed to get public IP'
     public_ip = f.read().strip()
 
@@ -32,7 +38,7 @@ def update_ip():
                         'ResourceRecordSet': {
                             'Name': DNS_NAME,
                             'Type': 'A',
-                            'TTL': ttl,
+                            'TTL': TTL_SECONDS,
                             'ResourceRecords': [
                                 { 'Value': public_ip }
                             ]
@@ -43,7 +49,7 @@ def update_ip():
                         'ResourceRecordSet': {
                             'Name': '*.' + str(DNS_NAME),
                             'Type': 'A',
-                            'TTL': ttl,
+                            'TTL': TTL_SECONDS,
                             'ResourceRecords': [
                                 { 'Value': public_ip }
                             ]
