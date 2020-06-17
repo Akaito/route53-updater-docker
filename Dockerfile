04b28a6b33f5033c565ed4b465e01380ffe73b08
Darren Gibbard <dgibbard@cisco.com>
2020-06-17 11:29:24 +0100

    Switch to python3, only update if changed etc

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,15 @@
 FROM alpine:latest
 MAINTAINER Chris Barrett <chris@codesaru.com>
 
-RUN apk --update add py2-pip \
+RUN apk --update add py3-pip \
 	&& pip install awscli \
+        && pip install iplookup \
 	&& rm -rf /var/cache/apk/*
 
 RUN addgroup -S route53 \
     && adduser -G route53 -S -D route53
 USER route53:route53
 
-#COPY aws /root/.aws
 COPY main.py /
 
-#CMD ["/bin/sh"]  # for debugging
 CMD [ "python", "./main.py" ]
-#CMD cron && tail -f /var/log/cron.log
-
--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-#!/usr/bin/python
+#!/bin/env python3
 
 import os
 import time
-import urllib
+import requests
+import socket
+from datetime import datetime
 
 import botocore
 import botocore.session
 
+from iplookup import iplookup
+
 # required env vars
-HOSTED_ZONE_ID = os.getenv('R53_HOSTED_ZONE_ID', None)  # TODO get config'd zone ID string (such as for codesaru.com)
-DNS_NAME       = os.getenv('DNS_NAME', None)  # TODO get from config (such as 'home.codesaru.com')
+R53_HOSTED_ZONE_ID = os.getenv('R53_HOSTED_ZONE_ID', None)
+DNS_NAME = os.getenv('DNS_NAME', None)
 
 # optional env vars
 PUBLIC_IP_URL = os.getenv('PUBLIC_IP_URL', 'http://checkip.amazonaws.com')
-TTL_SECONDS   = int(os.getenv('TTL_SECONDS', '300'))
+TTL_SECONDS = int(os.getenv('TTL_SECONDS', '300'))
 RETRY_SECONDS = TTL_SECONDS // 2
 
 
-assert HOSTED_ZONE_ID, "Route53's Hosted Zone ID should be set to env var HOSTED_ZONE_ID."
-assert DNS_NAME, "The DNS name to update A records for should be set to env var DNS_NAME."
+if not R53_HOSTED_ZONE_ID:
+    logit("ERROR", "Route53's Hosted Zone ID should be set to env var R53_HOSTED_ZONE_ID.")
+if not DNS_NAME:
+    logit("ERROR", "The DNS name to update A records for should be set to env var DNS_NAME.")
 
 
 # it's expected that you've volume-mounted a file with only the creds this container should use
@@ -26,52 +32,69 @@ session = botocore.session.get_session()
 route53_client = session.create_client('route53')
 
 
-def update_ip():
-    # get WAN IP
-    while True:
-        try:
-            f = urllib.urlopen(PUBLIC_IP_URL)
-            break
-        except IOError as e:
-            if e.errno == -3:  # "Try again"
-                print('Got IOError -3: "Try again".  Retrying after a sleep...')
-                time.sleep(RETRY_SECONDS)
-            else:
-                raise
-    assert f.getcode() == 200, 'Failed to get public IP'
-    public_ip = f.read().strip()
-    #print 'Discovered public IP: ' + public_ip
+def getdate():
+    return datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 
+
+def logit(level, message):
+    print(f'[{level.upper()}] {getdate()}: {message}')
+
+
+def is_valid_ipv4_address(address):
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  # no inet_pton here, sorry
+        try:
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count('.') == 3
+    except socket.error:  # not a valid address
+        return False
+
+    return True
+
+
+def update_ip():
+    # Get current DNS IP
+    ip = iplookup.iplookup
+    current_ip = ip(DNS_NAME)[0]
+    # get WAN IP
+    r = requests.get(PUBLIC_IP_URL)
+    public_ip = r.text.strip()
+    if not r.status_code == 200:
+        logit("WARN", f'Request to {PUBLIC_IP_URL} failed')
+    if not is_valid_ipv4_address(public_ip):
+        logit("WARN", f'Failed to receive valid IP: {public_ip}')
+
+    logit("INFO", f'Current DNS IP:\t{current_ip}')
+    logit("INFO", f'Current Public IP:\t{public_ip}')
+    # See if they're different
+    if public_ip == current_ip:
+        logit("INFO", f'No IP Change, skipping...')
+        return
+    logit("INFO", "Updating DNS...")
 
     # update Route 53
     response = route53_client.change_resource_record_sets(
-            HostedZoneId=HOSTED_ZONE_ID,
-            ChangeBatch={
-                'Comment': 'Update public IP address from Docker container.',
-                'Changes': [
-                    {
-                        'Action': 'UPSERT',
-                        'ResourceRecordSet': {
-                            'Name': str(DNS_NAME),
-                            'Type': 'A',
-                            'TTL': int(TTL_SECONDS),
-                            'ResourceRecords': [
-                                { 'Value': public_ip }
-                            ]
-                        }
-                    },
-                    {
-                        'Action': 'UPSERT',
-                        'ResourceRecordSet': {
-                            'Name': '*.' + str(DNS_NAME),
-                            'Type': 'A',
-                            'TTL': int(TTL_SECONDS),
-                            'ResourceRecords': [
-                                { 'Value': public_ip }
-                            ]
-                        }
+        HostedZoneId=R53_HOSTED_ZONE_ID,
+        ChangeBatch={
+            'Comment': 'Update public IP address from Docker container.',
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': str(DNS_NAME),
+                        'Type': 'A',
+                        'TTL': int(TTL_SECONDS),
+                        'ResourceRecords': [
+                            {'Value': public_ip}
+                        ]
                     }
-                ]})
+                }
+            ]
+        }
+    )
 
     print(response['ChangeInfo']['Status'])
 
@@ -82,4 +105,3 @@ if __name__ == '__main__':
     while os.getenv('KEEP_CONTAINER_ALIVE', 'False').lower() == 'true':
         time.sleep(int(TTL_SECONDS))
         update_ip()
-
